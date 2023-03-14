9052d35a0c609824c217ca7f88a160cc20db433d
yamir <yamir@mandragora.mandragora>
2023-03-14 18:07:11 +0100

    Arreglado ejercicio 11 para que use clone de arrays

--- src/TA06_11/TA06_11_App.java
+++ src/TA06_11/TA06_11_App.java
@@ -18,22 +18,24 @@ public class TA06_11_App {
 			int[] array2 = array1;
 			
 			// array1 y array2 son iguales porque uno apunta al otro
-			System.out.println("Arrays antes del cambio:");
+			System.out.println("Array2 apunta al array 1 por lo que siempre darán lo mismo a pesar de");
+			System.out.println("que el cambio de valor se realice mediante 1 de las dos variables:");
 			array1 = fillArrayConAleatorios(array1);
 			System.out.println("Array1:");
 			printIntArray(array1);
 			System.out.println("Array2:");
 			printIntArray(array2);
 			// array1 y array2 siguen con el mismo contenido porque apuntan a lo mismo
-			// pero el contenido ha cambiado
-			array2 = fillArrayConAleatorios(array2);
-			System.out.println("Arrays después del cambio:");
-			System.out.println("Array1:");
-			printIntArray(array1);
-			System.out.println("Array2:");
-			printIntArray(array2);
-			int[] array3 = multiplyArraysComponents(array1, array2);
-			System.out.println("Array3(multiplicado array1[i] * array2[i]):");
+			// a menos que usemos el método clone
+			int[] array22 = new int[array1.length];
+			array22 = array1.clone();
+			array22 = fillArrayConAleatorios(array22);
+			System.out.println("Se ha creado el array22 que contendrá otros valores aleatorios.");
+			System.out.println("Que no interferirá con el array1");
+			int[] array3 = multiplyArraysComponents(array1, array22);
+			System.out.println("Array22:");
+			printIntArray(array22);
+			System.out.println("Array3(multiplicado array1[i] * array22[i]):");
 			printIntArray(array3);
 		}
 	}
